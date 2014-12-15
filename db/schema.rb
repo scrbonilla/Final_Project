d4c37939f14e1f3fbf1cd5c5a3612fa50a318bc3
scrbonilla <scrbonilla84@gmail.com>
2014-12-15 15:53:47 -0500

    rake db:migrate deals schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215143255) do
+ActiveRecord::Schema.define(version: 20141215194420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deals", force: true do |t|
+    t.string   "img"
+    t.string   "url"
+    t.string   "title"
+    t.string   "description"
+    t.decimal  "deal_price",   precision: 8, scale: 2
+    t.decimal  "retail_price", precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
